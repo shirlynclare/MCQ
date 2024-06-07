e84b79e83f7e7343c9abb5ed151dcaaf40c31536
shirlynclare <168082416+shirlynclare@users.noreply.github.com>
2024-06-07 16:05:36 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-#install packages meaning
+#install packages needed
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='sunny savita',
+    author_email='sunny.savita@ineuron.ai',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
